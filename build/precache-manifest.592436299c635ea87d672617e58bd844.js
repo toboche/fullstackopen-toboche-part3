1154bd17b2f851daea79439319e839a966b02e61
toboche <toboche@gmail.com>
2020-10-03 20:57:29 +0200

    uibuild

---
+++ build/precache-manifest.592436299c635ea87d672617e58bd844.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b51b3aafb9e644d064d6474fc1b2b2da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1824a0b333bee937dfba",
+    "url": "/static/css/main.79023a80.chunk.css"
+  },
+  {
+    "revision": "54dd32709c192453a110",
+    "url": "/static/js/2.d98781c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d98781c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1824a0b333bee937dfba",
+    "url": "/static/js/main.68195501.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
