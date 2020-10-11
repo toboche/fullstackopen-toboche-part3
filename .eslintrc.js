199b46c2a425a70ebc6ac5c303572a658655f85a
toboche <toboche@gmail.com>
2020-10-11 13:09:15 +0200

    3.21

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'commonjs': true,
+        'es2021': true
+    },
+    'extends': 'eslint:recommended',
+    'parserOptions': {
+        'ecmaVersion': 12
+    },
+    'rules': {
+        'no-unused-vars': [2, {'argsIgnorePattern': '^_'}],
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'never'
+        ]
+    }
+}
